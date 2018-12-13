20977f2e9dac3244d5842c31b1e0b854dad63a5e
Logan Absher <absherlogan@gmail.com>
2018-12-13 13:28:23 -0800

    checkout

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(express.static(`${__dirname}/build`));
+
 app.get('/*', ((req, res) => res.redirect('/')));
 
 app.listen(PORT, () => console.log('running on port:', PORT));
